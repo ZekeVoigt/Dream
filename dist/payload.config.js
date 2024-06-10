b4ea37b506487ef22fa2cebdddbcc6d0c836500c
Zeke Voigt <Zekevoigt@gmail.com>
2024-06-10 15:17:28 -0500

    yes please	modified:   package.json

--- dist/payload.config.js
+++ dist/payload.config.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 var config_1 = require("payload/config");
 var bundler_webpack_1 = require("@payloadcms/bundler-webpack");
@@ -15,31 +17,37 @@ var ProductFile_1 = require("./collections/ProductFile");
 var Orders_1 = require("./collections/Orders");
 var Media_1 = require("./collections/Media");
 dotenv_1.default.config({
-    path: path_1.default.resolve(__dirname, "../.env"),
+  path: path_1.default.resolve(__dirname, "../.env"),
 });
 exports.default = (0, config_1.buildConfig)({
-    serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-    collections: [Users_1.Users, Products_1.Products, Media_1.Media, ProductFile_1.ProductFiles, Orders_1.Orders],
-    routes: {
-        admin: "/sell",
+  serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
+  collections: [
+    Users_1.Users,
+    Products_1.Products,
+    Media_1.Media,
+    ProductFile_1.ProductFiles,
+    Orders_1.Orders,
+  ],
+  routes: {
+    admin: "/sell",
+  },
+  admin: {
+    user: "users",
+    bundler: (0, bundler_webpack_1.webpackBundler)(),
+    meta: {
+      titleSuffix: "- Dream",
+      favicon: "/favicon.ico",
+      ogImage: "/thumbnail.jpg",
     },
-    admin: {
-        user: "users",
-        bundler: (0, bundler_webpack_1.webpackBundler)(),
-        meta: {
-            titleSuffix: "- Dream",
-            favicon: "/favicon.ico",
-            ogImage: "/thumbnail.jpg",
-        },
-    },
-    rateLimit: {
-        max: 2000,
-    },
-    editor: (0, richtext_slate_1.slateEditor)({}),
-    db: (0, db_mongodb_1.mongooseAdapter)({
-        url: process.env.MONGODB_URL,
-    }),
-    typescript: {
-        outputFile: path_1.default.resolve(__dirname, "payload-types.ts"),
-    },
+  },
+  rateLimit: {
+    max: 2000,
+  },
+  editor: (0, richtext_slate_1.slateEditor)({}),
+  db: (0, db_mongodb_1.mongooseAdapter)({
+    url: process.env.MONGODB_URL,
+  }),
+  typescript: {
+    outputFile: path_1.default.resolve(__dirname, "payload-types.ts"),
+  },
 });
