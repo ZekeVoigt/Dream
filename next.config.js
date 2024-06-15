a474b0cf332fec7007b6b21ceb00b2ebd22d6648
Zeke Voigt <Zekevoigt@gmail.com>
2024-06-14 23:50:29 -0500

    push changes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ["localhost", "dream-git-main-zeke-voigts-projects.vercel.app"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
